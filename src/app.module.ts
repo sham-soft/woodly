daddc94d2f9ce4c1d91ba7ee100f69e98f21a64c
sh.tepsurkaev <shamil.tepsurkaev@simbirsoft.com>
2024-01-30 23:31:01 +0400

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
+// import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PaymentsModule } from './payments/payments.module';
+// import { PaymentsModule } from './payments/payments.module';
 
 @Module({
-    imports: [PaymentsModule, MongooseModule.forRoot('mongodb://localhost:27017/woodly')],
+    // imports: [PaymentsModule, MongooseModule.forRoot('mongodb://localhost:27017/woodly')],
     controllers: [AppController],
     providers: [AppService],
 })
