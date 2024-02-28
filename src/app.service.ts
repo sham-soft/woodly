685d49581219c2ba4dfc436579120ec7afce4884
IMRITOS <imran.tepsurkaev@yandex.ru>
2024-02-29 00:33:39 +0400

    asd

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
     getHello(): string {
-        return 'kkВас приветствует сервис обработки платежей Woodly!';
+        return 'bvvВас приветствует сервис обработки платежей Woodly!';
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,15 @@ import helmet from 'helmet';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    app.use(helmet({
-        crossOriginEmbedderPolicy: false,
-        crossOriginResourcePolicy: false,
-    }));
+    // app.use(helmet({
+    //     crossOriginEmbedderPolicy: false,
+    //     crossOriginResourcePolicy: false,
+    // }));
+    app.enableCors({
+        origin: '*',
+        methods: 'GET, PUT, POST, DELETE',
+        allowedHeaders: 'Content-Type, Authorization',
+    });
     await app.listen(3000);
 }
 bootstrap();
