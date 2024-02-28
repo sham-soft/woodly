ae2d693d36965620d8bb78fcd75f2c3690036e02
IMRITOS <imran.tepsurkaev@yandex.ru>
2024-02-29 01:16:06 +0400

    asd

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import helmet from 'helmet';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    // app.use(helmet({
-    //     crossOriginEmbedderPolicy: false,
-    //     crossOriginResourcePolicy: false,
-    // }));
-    app.enableCors({
-        origin: "*",
-    })
     await app.listen(3000);
 }
 bootstrap();
