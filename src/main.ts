ec4e1c52650012646e69b6c3f0ed05ef3f3a4873
IMRITOS <imran.tepsurkaev@yandex.ru>
2024-02-29 00:02:59 +0400

    up

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const whitelist = [
-    'http://localhost:3000',
-    'http://localhost:5173',
-];
-
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    app.enableCors({
-        allowedHeaders: ['content-type'],
-        origin: 'http://localhost:5173',
-        credentials: true,
-    });
     await app.listen(3000);
 }
 bootstrap();
