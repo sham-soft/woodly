7c0455ed51868099fa162862db0a8691d03645b4
IMRITOS <imran.tepsurkaev@yandex.ru>
2024-02-29 23:22:47 +0400

    asfg

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    app.enableCors();
+    // app.enableCors();
     // app.enableCors({
     //     origin: '*',
     //     methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
