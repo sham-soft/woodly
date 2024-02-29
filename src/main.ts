d711495db82cd3f666068fc799afa2c08cbec4b8
IMRITOS <imran.tepsurkaev@yandex.ru>
2024-02-29 22:16:23 +0400

    asdf

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    app.use(cors())
+    app.enableCors({
+        origin: ['http://localhost:5173/', 'https://woodly-azure.vercel.app', 'http://192.168.0.105:5173'],
+    });
     await app.listen(3000);
 }
 bootstrap();
