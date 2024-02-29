9b1487e05f052a516805ce7a112f06b9186e9c6f
IMRITOS <imran.tepsurkaev@yandex.ru>
2024-02-29 22:43:37 +0400

    asvbb

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
     getHello(): string {
-        return 'Вас приветствует сервис обработки платежей Woodly!';
+        return '234Вас приветствует сервис обработки платежей Woodly!';
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors({
-        origin: ['http://localhost:5173', 'https://woodly-azure.vercel.app', 'http://192.168.0.105:5173'],
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+        origin: ['https://woodly-azure.vercel.app', 'http://localhost:5173', 'http://192.168.0.105:5173'],
+        methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
+        credentials: true,
+        allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
     });
     await app.listen(3000);
 }
