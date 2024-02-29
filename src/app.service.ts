f811e004b48beaed50975965e25c61dbf3c102de
IMRITOS <imran.tepsurkaev@yandex.ru>
2024-02-29 22:52:25 +0400

    asf

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
     getHello(): string {
-        return '234Вас приветствует сервис обработки платежей Woodly!';
+        return '444Вас приветствует сервис обработки платежей Woodly!';
     }
 }
