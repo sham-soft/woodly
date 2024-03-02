22c5d28e54707cb0c4e78979df92c712ac44a9ff
IMRITOS <imran.tepsurkaev@yandex.ru>
2024-03-02 16:41:49 +0400

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,8 +9,8 @@ import { CardsModule } from './cards/cards.module';
     imports: [
         CardsModule,
         // TransactionsModule,
-        MongooseModule.forRoot('mongodb://localhost:27017/woodly'),
-        // MongooseModule.forRoot('mongodb+srv://code-build:code-build@cluster0.3bdan.mongodb.net/woodly'),
+        // MongooseModule.forRoot('mongodb://localhost:27017/woodly'),
+        MongooseModule.forRoot('mongodb+srv://code-build:code-build@cluster0.3bdan.mongodb.net/woodly'),
     ],
     controllers: [AppController],
     providers: [AppService],
