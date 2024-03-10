51f6452da49eb4490354c4446380dce10d312238
IMRITOS <imran.tepsurkaev@yandex.ru>
2024-03-10 12:13:44 +0400

    add transactions module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CardsModule } from './cards/cards.module';
-// import { TransactionsModule } from './transactions/transactions.module';
+import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
     imports: [
         CardsModule,
-        // TransactionsModule,
+        TransactionsModule,
         // MongooseModule.forRoot('mongodb://localhost:27017/woodly'),
         MongooseModule.forRoot('mongodb+srv://code-build:code-build@cluster0.3bdan.mongodb.net/woodly'),
     ],
