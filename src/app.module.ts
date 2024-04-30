872ab80b6c6a19f285bf7b45825ca45a45bb6567
IMRITOS <imran.tepsurkaev@yandex.ru>
2024-04-30 18:47:56 +0400

    change db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ScheduleModule } from '@nestjs/schedule';
+// import { AuthModule } from './modules/auth/auth.module';
 import { AutopaymentsModule } from './modules/autopayments/autopayments.module';
 import { BalanceModule } from './modules/balance/balance.module';
 import { CardsModule } from './modules/cards/cards.module';
@@ -16,6 +17,7 @@ import { AppService } from './app.service';
 
 @Module({
     imports: [
+        // AuthModule,
         AutopaymentsModule,
         BalanceModule,
         CardsModule,
@@ -27,8 +29,8 @@ import { AppService } from './app.service';
         TransactionsModule,
         UsersModule,
         ScheduleModule.forRoot(),
-        MongooseModule.forRoot('mongodb://localhost:27017/woodly'),
-        // MongooseModule.forRoot('mongodb+srv://code-build:code-build@cluster0.3bdan.mongodb.net/woodly'),
+        // MongooseModule.forRoot('mongodb://localhost:27017/woodly'),
+        MongooseModule.forRoot('mongodb+srv://code-build:code-build@cluster0.3bdan.mongodb.net/woodly'),
     ],
     controllers: [AppController],
     providers: [AppService],
